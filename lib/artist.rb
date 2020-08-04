4b2689d66182a8b5045b1b635632d4842e6bd194
ariel murphy <arielladders@gmail.com>
2020-08-04 02:42:02 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist 
+  attr_accessor :name, :genre, :songs 
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end 
+  
+  def songs 
+    Song.all.select do |song| 
+      song.artist.name == self.name 
+    end 
+  end 
+  
+  def genres 
+    Song.all.collect {|song| song.genre} 
+  end 
